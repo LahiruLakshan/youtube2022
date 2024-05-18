44122492ff35211393cff70b18cf687cbe4b8862
LahiruLakshan <balahirulakshan@gmail.com>
2024-05-19 02:30:51 +0530

    setup firebase findgrapher

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "chat-ab746.firebaseapp.com",
-  projectId: "chat-ab746",
-  storageBucket: "chat-ab746.appspot.com",
-  messagingSenderId: "901216368405",
-  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+  apiKey: "AIzaSyAc9cqMTFoW0dbt8sin2wAojea5v66eYQU",
+  authDomain: "findgrapher-2b5bc.firebaseapp.com",
+  projectId: "findgrapher-2b5bc",
+  storageBucket: "findgrapher-2b5bc.appspot.com",
+  messagingSenderId: "273440929528",
+  appId: "1:273440929528:web:6685798caa5101bfcead0d",
+  measurementId: "G-DE81JG6LLH"
 };
 
 // Initialize Firebase
